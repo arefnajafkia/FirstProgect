46deea8bd485bb92e4ea759e3b0e423fa37db9ed
aref najafkia <arefnajafkia294@gmail.com>
2022-04-28 23:06:02 +0430

    Added page2.py for phone2

--- page.py/page2.py
+++ page.py/page2.py
@@ -1,67 +1,75 @@
 
-#barname phone ba save and delet and bacgrand meshge
+from asyncore import read
 from logging import root
+from msilib.schema import Error, ListBox
+from select import select
 from tkinter import messagebox
 from tkinter import *
+from unicodedata import name
 import webbrowser
+from importlib_metadata import entry_points
+from matplotlib.pyplot import get
 import pyperclip
 import random
 import os
 
 
+
 root = Tk()
-root.title('Coontact Book')
-root.geometry('650x300')
+root.title('Phone Book')
+root.geometry('1000x400')
 background = '#121212'
-root.config(bg=background)
+root.config(bg= background)
 
-#contact Name label✔
-#contact Name Entry
+def Finding_btn():
 
-#contact Number label
-#contact Number Entry
+    with open('D:\Visual Studio\MyProject\page.py\page1\saved_Name.txt','r') as f:
 
-#Add contact Button
-#Save list Button
+        khotut = f.readlines()
+        for line in khotut:             
+            
+            line = name_entry.get() + ': ' + phone_entry.get() + ': ' + Mobile_entry.get() + ': '  + Address_entry.get() + ': ' + Maill_entry.get()
+            listbox.insert(END,line, '     ====  welcome   It was your request   Thank you  ====  ')
 
-#copy phone Number Button
-#Open Saved Fille Button
-#Delete Contact Button
-#Exit App Button
-# ListBox  For Contacts
+        
+            
+            name_entry.append(item)(0, END),
+            phone_entry.append(item)(0, END),
+            Mobile_entry.append(item)(0, END),
+            Address_entry.append(item)(0, END),
+            Maill_entry.append(item)(0, END)           
+            
 
-# Functons
+
 def add_contact() :
-    contact_string = name_entry.get() + ': ' + phone_entry.get()
-    listbox.insert(END, contact_string)
+    contact_string = name_entry.get() + ': ' + phone_entry.get() + ': ' + Mobile_entry.get() + ': '  + Address_entry.get() + ': ' + Maill_entry.get() 
+     
+    listbox.insert(END,  contact_string)
     
     name_entry.delete(0, END)
     phone_entry.delete(0, END)
+    Mobile_entry.delete(0, END)
+    Address_entry.delete(0, END)
+    Maill_entry.delete(0, END)
     
 def delete_contact():
-        listbox.delete(ANCHOR)
-        
+    listbox.delete(ANCHOR)
+    
 def save_list():
     """ Save the list to a simple txt file """
             
-    with open('D:\Visual Studio\MYPROJECT\Contact Book\saved.txt','w') as  f:
+    with open('D:\Visual Studio\MyProject\page.py\page1\saved_Name.txt','w') as  f:
           list_tuple = listbox.get(0, END)
           for item in list_tuple:
                if item.endswith('\n'):
                     f.write(item)
                else:
                     f.write(item+'\n')
+                    
 def open_list():
-        with open('D:\Visual Studio\MYPROJECT\Contact Book\saved.txt','r') as  f:
+        with open('D:\Visual Studio\MyProject\page.py\page1\saved_Name.txt','r') as  f:
             for line in f:
                 listbox.insert(END, line)
-def open_dir():
-        webbrowser.open('D:\Visual Studio\MYPROJECT\Contact Book')  
-
-def copy_number():
-        selected_contact = listbox.get(ANCHOR)
-        number = selected_contact.split(': ')
-        pyperclip.copy(number[1].replace('\n', ""))         
 
 def exit():
           choice = messagebox.askquestion('Exit Aplictation', 'Are you sure you want to ciose the application ?')
@@ -69,43 +77,61 @@ def exit():
              root.destroy()
           else:
                return 
-    
-#labellist For Contact
+
+
 name_label = Label(root, text='Contact Name:', bg=background,  fg='white',  font=('calibri',12), anchor='w', justify=LEFT )
 name_label. place(relx=0.1, rely=0.1, anchor='c')
 
-name_entry = Entry(root, bg='white', fg=background, width=30, borderwidth=2)
-name_entry. place(relx=0.4, rely=0.1, anchor='c')
+name_entry = Entry(root, bg='white', fg=background, width=40, borderwidth=4)
+name_entry. place(relx=0.3, rely=0.1, anchor='c')
 
-phone_label = Label(root, text='   Contact:', bg=background,  fg='white',  font=('calibri',12), anchor='w', justify=LEFT )
+phone_label = Label(root, text='Contact phone:', bg=background,  fg='white',  font=('calibri',12), anchor='w', justify=LEFT )
 phone_label. place(relx=0.1, rely=0.2, anchor='c')
 
 
-phone_entry = Entry(root, bg='white', fg=background, width=30, borderwidth=2)
-phone_entry. place(relx=0.4, rely=0.2, anchor='c')
+phone_entry = Entry(root, bg='white', fg=background, width=40, borderwidth=4)
+phone_entry. place(relx=0.3, rely=0.2, anchor='c')
 
-add_btn = Button(root, text='Add Contact',  bg=background, fg='white', borderwidth=3, padx=125,command=add_contact )
-add_btn.place(relx=0.29, rely=0.35, anchor='c')
+Mobile_label = Label(root, text=' Contact Mobile:', bg=background,  fg='white',  font=('calibri',12), anchor='w', justify=LEFT )
+Mobile_label. place(relx=0.1, rely=0.3, anchor='c')
 
-save_btn = Button(root, text='Save List',  bg=background, fg='white',borderwidth=3, padx=135, command=save_list)
-save_btn. place(relx=0.29, rely=0.5, anchor='c')
 
-copyPhone = Button(root, text='Copy Phone Number',  bg=background, fg='white',borderwidth=3, padx=10, command=copy_number )
-copyPhone. place(relx=0.15, rely=0.65, anchor='c')
+Mobile_entry = Entry(root, bg='white', fg=background, width=40, borderwidth=4)
+Mobile_entry. place(relx=0.3, rely=0.3, anchor='c')
 
-deletePhone = Button(root, text='Delete Contact',  bg=background, fg='white', borderwidth=3, padx=28 ,  command=delete_contact)
-deletePhone.place(relx=0.15, rely=0.77, anchor='c')
+Address_label = Label(root, text='  Contact Address:', bg=background,  fg='white',  font=('calibri',12), anchor='w', justify=LEFT )
+Address_label. place(relx=0.1, rely=0.4, anchor='c')
 
-openSaved = Button(root, text='Open Saved File',  bg=background, fg='white',borderwidth=3, padx=30, command=open_dir )
-openSaved.place(relx=0.42, rely=0.65, anchor='c')
 
-exit = Button(root, text='Exit App',  bg=background, fg='white',borderwidth=3, padx=50, command=exit)
-exit.place(relx=0.42, rely=0.77, anchor='c')
+Address_entry = Entry(root, bg='white', fg=background, width=40, borderwidth=4)
+Address_entry. place(relx=0.3, rely=0.4, anchor='c')
 
-listbox = Listbox(root, width=40, height=15)
-listbox.place(relx=0.75, rely=0.47, anchor='c')
+Maill_label = Label(root, text='Contact Maill :', bg=background,  fg='white',  font=('calibri',12), anchor='w', justify=LEFT )
+Maill_label. place(relx=0.1, rely=0.5, anchor='c')
 
+
+Maill_entry = Entry(root, bg='white', fg=background, width=40, borderwidth=4)
+Maill_entry. place(relx=0.3, rely=0.5, anchor='c')
+
+Finding_btn = Button(root, text='Finding contact',  bg=background, fg='white', borderwidth=3, padx=160,command=Finding_btn )
+Finding_btn. place(relx=0.25, rely=0.60, anchor='c')
+
+add_btn = Button(root, text='Add Contact',  bg=background, fg='white', borderwidth=3, padx=68,command=add_contact )
+add_btn.place(relx=0.15, rely=0.70, anchor='c')
+
+save_btn = Button(root, text='Save List',  bg=background, fg='white',borderwidth=3, padx=82,command=save_list )
+save_btn. place(relx=0.37, rely=0.70, anchor='c')
+
+deletePhone = Button(root, text='Delete Contact',  bg=background, fg='white', borderwidth=3, padx=177 ,  command=delete_contact)
+deletePhone.place(relx=0.26, rely=0.80, anchor='c')
+
+exit = Button(root, text='Exit App',  bg=background, fg='white',borderwidth=3, padx=194, command=exit )
+exit.place(relx=0.26, rely=0.90, anchor='c')
+
+listbox = Listbox(root, width=90, height=22)
+listbox.place(relx=0.71, rely=0.49, anchor='c')
 
 
 open_list()
 root.mainloop ()
+
